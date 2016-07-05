4ee62cd9b3efaee2e5fa8cbbf5235718b3ea94b0
Tukonra <tmelkov@yandex.ru>
2016-07-05 18:26:36 +0300

    Добавлен модуль C5.1 (еще не закончен)

---
+++ С5.1/С5.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace С5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            int[,] a = new int[n, k];
+
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    a[i, j] = random.Next(100);
+                    Console.Write("{0,4}", a[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
