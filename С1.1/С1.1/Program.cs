aaaa040067bc6315c1306939275a28d33699e869
Tukonra <tmelkov@yandex.ru>
2016-07-05 18:01:22 +0300

    Добавлен модуль C1.1 (еще не закончен)

---
+++ С1.1/С1.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace С1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y, y1;
+            int cnt = 0;
+
+            Console.WriteLine("Введите первое число");                 //Ввод числа x
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");                 //Ввод числа a
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число");
+            int e = Convert.ToInt32(Console.ReadLine());
+            
+            y = a;
+            y1 = y;
+
+            double k = Math.Abs(y * y - y1 * y1);
+
+            for (int i = 1; k < e; i++)
+            {
+                if (k < e)
+                {
+                    y1 = y;
+                    y = 0.5 * (y1 + x / y1);
+                    cnt = e;
+                }
+            }
+            Console.Write("Первый член последовательности - ");
+            Console.WriteLine(cnt);
+            Console.ReadKey();
+        }
+    }
+}
