708992fc094a6eb6129be3003be1b7d28dd3b51c
Tukonra <tmelkov@yandex.ru>
2016-07-05 18:04:36 +0300

    Добавлен модуль C3.2 (еще не закончен)

---
+++ С3.2/С3.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace С3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            int[] x = new int[] { 1, 2 };
+            int[] y = new int[] { 1, 2 };
+            int[] arr = new int[] { 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25 };
+            for (int i = 1; i < 26; i++);
+            {
+                
+            }
+        }
+    }
+}
