0bdb008a53d588230ce55a4bc4b385892aaf9904
Tukonra <tmelkov@yandex.ru>
2016-07-05 17:54:05 +0300

    Добавлен модуль C2.1

---
+++ С2.1/C2.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double summ = 0;
+            int k = 0;
+
+            Console.Write("Введите число n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n <= 0) Console.WriteLine("Значение введено некорректно!");
+
+            double[] array = new double[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)                       
+            {
+                array[i] = (rnd.NextDouble()-0.5)*100;                              //Заполнение массива
+
+                if (array[i] < 2) array[i] = 0;                                     //Замена элемента < 2 на 0
+
+                if (array[i] >= 3 && array[i] <= 7)                                 //Сумма элементов и их число на отрезке [3;7]
+                {
+                    summ += array[i];
+                    k += 1;
+                }
+                Console.WriteLine(array[i]);
+            }
+
+            Console.WriteLine();
+            Console.Write("Сумма элементов последовательности: " + summ);
+            Console.Write("\nЧисло элементов, принадлежащих отрезку [3,7] = " + k);
+            Console.Read();
+        }
+    }
+}
