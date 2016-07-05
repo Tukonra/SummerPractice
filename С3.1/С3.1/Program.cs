6fe36b1e72ba0a635a8bb766ef0a8b855203cab4
Tukonra <tmelkov@yandex.ru>
2016-07-05 18:03:58 +0300

    Добавлен модуль C3.1 (еще не закончен)

---
+++ С3.1/С3.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace С3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число n");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            char[] array = new char[n];                                           //Объявление массива
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToChar(rnd.Next('*' , '*' + 27));              //Заполнение массива
+                Console.Write("{0} ", array[i]);
+
+                if  (array[i] == '*') array[i] = ' ';                              //Замена элемента
+                else  Console.Write(array[i]);                                    //Повторение элемента
+               
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
