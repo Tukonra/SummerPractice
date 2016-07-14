f191aa7ecd80d47301ae06c6c956142757756c84
Tukonra <tmelkov@yandex.ru>
2016-07-14 09:59:27 +0300

    Добавлен модуль C4.1 (готово к проверке)

---
+++ С4.1/С4.1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace С4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символьную строку"); ;
+            string text = Convert.ToString(Console.ReadLine());
+
+            List<char> chars = new List<char>();
+
+            int c, ff, sf;
+            bool bf, af;
+
+            bool f;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                c = 0;
+                ff = -1;
+                sf = -1;
+                af = false;
+                bf = false;
+
+                for (int j = 0; j < text.Length; j++)
+                {
+                    if (ff != -1)
+                    {
+                        if (text[j] == 'a')
+                            af = true;
+
+                        if (text[j] == 'b')
+                            bf = true;
+                    }
+
+                    if (text[i] == text[j])
+                    {
+                        c++;
+
+                        if (ff == -1)
+                            ff = text[j];
+
+                        sf = text[j];
+                    }
+                }
+
+                if (c > 1 && bf && af)
+                {
+                    f = false;
+
+                    foreach (char ch in chars)
+                        if (ch == text[i])
+                            f = true;
+
+                    if (!f)
+                        chars.Add(text[i]);
+                }
+            }
+
+            foreach (char ch in chars)
+                Console.WriteLine(ch);
+
+            Console.ReadKey();
+        }
+    }
+}
