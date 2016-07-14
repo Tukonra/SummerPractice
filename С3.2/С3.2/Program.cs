93818587d1baeae76e5f439e70bc245b9c371b90
Tukonra <tmelkov@yandex.ru>
2016-07-14 09:06:24 +0300

    Доделан модуль C3.2 (Готово к проверке)

--- С3.2/С3.2/Program.cs
+++ С3.2/С3.2/Program.cs
@@ -8,15 +8,50 @@ namespace С3._2
 {
     class Program
     {
-        static void Main(string[] args)
-        {           
-            int[] x = new int[] { 1, 2 };
-            int[] y = new int[] { 1, 2 };
-            int[] arr = new int[] { 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25 };
-            for (int i = 1; i < 26; i++);
+        static void Main(string[] args) //280
+        {
+            int b = 0;
+            float[] x = new float[26];
+            float[] y = new float[26];
+
+            x[1] = 1;                     //Задаем начальные данные
+            y[1] = 1;
+            x[2] = 2;
+            y[2] = 2;
+
+            for (int i = 1; i <= 25; i++)
             {
-                
+                if (i > 2)
+                {
+                    x[i] = Program.getX(i, y[i - 1], y[i - 2]);
+                    y[i] = Program.getY(i, x[i - 1], x[i - 2], y[i - 1]);
+                }
+
+                b++;
+                Console.Write("" + b + " ");
+                Console.Write(x[i]);
+                Console.Write(", ");
+                Console.Write("" + b + " ");
+                Console.Write(y[i]);
+                Console.WriteLine();
             }
+
+            Console.ReadKey();
+        }
+
+        static float getX(int i, float y1, float y2)
+        {
+            return (y1 - y2) / i;
+        }
+
+        static float getY(int i, float x1, float x2, float y1)
+        {
+            return (x1 * x1 + x2 + y1) / Program.factorial(i);
+        }
+
+        static int factorial(int i)
+        {
+            return (i == 0) ? 1 : (i * Program.factorial(i - 1));
         }
     }
 }
